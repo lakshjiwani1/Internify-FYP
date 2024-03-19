8d652c7bbd9a5080a026058e0a94a999c877aa50
Sameer Batra <sameerbatra1@gmail.com>
2024-03-19 22:55:13 +0500

    Delete students directory

--- students/forms.py
+++
@@ -1,6 +0,0 @@
-from django import forms
-
-class InternshipSearchForm(forms.Form):
-    search_by = forms.ChoiceField(choices=[('company_name', 'Company Name'), ('internship_name', 'Internship Name')])
-    query = forms.CharField()
-    
